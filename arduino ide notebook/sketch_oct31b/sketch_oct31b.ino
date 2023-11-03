bddcc50b87fb45c800e5291c16accb72c3d195ed
Evan Rovelli <erovelli@umass.edu>
2023-11-02 22:03:08 -0400

    wav to speaker output trial1

---
+++ arduino ide notebook/sketch_oct31b/sketch_oct31b.ino
@@ -0,0 +1,17 @@
+#include <BufferedPrint.h>
+#include <FreeStack.h>
+#include <MinimumSerial.h>
+#include <RingBuf.h>
+#include <SdFat.h>
+#include <SdFatConfig.h>
+#include <sdios.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
