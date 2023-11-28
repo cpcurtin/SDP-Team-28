9ae38e9e8c8a0454aedc0914c6a51f730ace4dd4
rgordon22 <ryamgordon@umass.edu>
2023-11-27 22:21:42 -0500

    Button_Matrix

--- Button_Matrix/Button_Matrix.ino
+++ Button_Matrix/Button_Matrix.ino
@@ -1,25 +1,47 @@
 
-byte rows[] = {5,6};
+byte rows[] = {4,5};
 const int rowCount = sizeof(rows)/sizeof(rows[0]);
 
 // JP2 and JP3 are outputs
-byte cols[] = {2,3,4};
+byte cols[] = {2,3};
 const int colCount = sizeof(cols)/sizeof(cols[0]);
 
 byte keys[colCount][rowCount];
+
+
+byte LED_rows[] = {10,9};
+const int LED_rowCount = sizeof(rows)/sizeof(rows[0]);
+
+// JP2 and JP3 are outputs
+byte LED_cols[] = {6,7};
+const int LED_colCount = sizeof(cols)/sizeof(cols[0]);
 
 void setup() {
   // put your setup code here, to run once:
 	Serial.begin(9600);
 
 	for(int x=0; x<rowCount; x++) {
-		Serial.print(rows[x]); Serial.println(" as input");
+		//Serial.print(rows[x]); Serial.println(" as input");
 		pinMode(rows[x], INPUT_PULLUP);
 	}
 
 	for (int x=0; x<colCount; x++) {
-		Serial.print(cols[x]); Serial.println(" as input-pullup");
+	//	Serial.print(cols[x]); Serial.println(" as input-pullup");
 		pinMode(cols[x], INPUT_PULLUP);
+  
+
+	}
+
+	for(int x=0; x<LED_rowCount; x++) {
+		//Serial.print(rows[x]); Serial.println(" as input");
+		pinMode(LED_rows[x], OUTPUT);
+    digitalWrite(LED_rows[x], HIGH);
+	}
+
+	for (int x=0; x<LED_colCount; x++) {
+	//	Serial.print(cols[x]); Serial.println(" as input-pullup");
+		pinMode(LED_cols[x], OUTPUT);
+    digitalWrite(LED_cols[x], LOW);
   
 
 	}
@@ -33,6 +55,7 @@ void readMatrix() {
 		byte curCol = cols[colIndex];
 		pinMode(curCol, OUTPUT);
 		digitalWrite(curCol, LOW);
+  
 
 		// row: interate through the rows
 		for (int rowIndex=0; rowIndex < rowCount; rowIndex++) {
@@ -44,15 +67,26 @@ void readMatrix() {
         Serial.println("Push");
         Serial.print(rowIndex);
         Serial.print(colIndex);
-      
+        digitalWrite(LED_cols[colIndex], HIGH);
+        digitalWrite(LED_rows[rowIndex], LOW);
 
       }
+
+      else{
+        digitalWrite(LED_cols[colIndex], LOW);
+        digitalWrite(LED_rows[rowIndex], HIGH);
+
+
+      }
+
 
 		}
 
 		// disable the column
     //delay(1000);
 		pinMode(curCol, INPUT_PULLUP);
+
+
 
     
 	}
