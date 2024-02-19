7feb4d069bfc7b21970ddc1937acecf45d5ef368
rgordon22 <ryamgordon@umass.edu>
2024-02-19 00:22:57 -0500

    Schematic pinout update

--- main/main.h
+++ main/main.h
@@ -15,39 +15,39 @@
  *    P:  CAN
  **************************  HEADERS  **************************
  *    PINOUT  R   O   Y           G     B   I     P     | PINOUT
- *    PIN0:   PWM                 CS1   RX1       CRX2  | PIN0:
- *    PIN1:   PWM                 MISO1 TX1       CTX2  | PIN1:
- *    PIN2:   PWM     OUT2                              | PIN2:
- *    PIN3:   PWM     LRCLK2                            | PIN3:
- *    PIN4:   PWM     BCLK2                             | PIN4:
- *    PIN5:   PWM     IN2                               | PIN5:
+ *    PIN0:   PWM                 CS1   RX1       CRX2  | PIN0: empty(For MIDI)
+ *    PIN1:   PWM                 MISO1 TX1       CTX2  | PIN1: MIDI
+ *    PIN2:   PWM     OUT2                              | PIN2: DPAD
+ *    PIN3:   PWM     LRCLK2                            | PIN3: DPAD
+ *    PIN4:   PWM     BCLK2                             | PIN4: DPAD
+ *    PIN5:   PWM     IN2                               | PIN5: DPAD
  *    PIN6:   PWM     OUT1D                             | PIN6:
- *    PIN7:   PWM     OUT1A             RX2             | PIN7:
- *    PIN8:   PWM     IN1               TX2             | PIN8:
- *    PIN9:   PWM     OUT1C                             | PIN9:
- *    PIN10:  PWM     MQSR        CS                    | PIN10:
- *    PIN11:  PWM                 MOSI            CTX1  | PIN11:
- *    PIN12:  PWM     MQSL        MISO                  | PIN12:
+ *    PIN7:   PWM     OUT1A             RX2             | PIN7: DAC
+ *    PIN8:   PWM     IN1               TX2             | PIN8: DEC3
+ *    PIN9:   PWM     OUT1C                             | PIN9: MIDI
+ *    PIN10:  PWM     MQSR        CS                    | PIN10: DEC3
+ *    PIN11:  PWM                 MOSI            CTX1  | PIN11: DEC3
+ *    PIN12:  PWM     MQSL        MISO                  | PIN12: DEC3
  *    PIN13:  PWM                 SCK                   | PIN13:
  *    PIN14:  PWM A0  S/PDIF OUT        TX3             | PIN14:
  *    PIN15:  PWM A1  S/PDIF IN         RX3             | PIN15:
  *    PIN16:      A2                    RX4 SCL1        | PIN16:
- *    PIN17:      A3                    TX4 SDA1        | PIN17:
- *    PIN18:  PWM A4                        SDA         | PIN18:
- *    PIN19:  PWM A5                        SCL         | PIN19:
- *    PIN20:      A6  LRCLK1            TX5             | PIN20:
- *    PIN21:      A7  BCLK1             RX5             | PIN21:
+ *    PIN17:      A3                    TX4 SDA1        | PIN17: 
+ *    PIN18:  PWM A4                        SDA         | PIN18: LCD
+ *    PIN19:  PWM A5                        SCL         | PIN19: LCD
+ *    PIN20:      A6  LRCLK1            TX5             | PIN20: DAC
+ *    PIN21:      A7  BCLK1             RX5             | PIN21: DAC
  *    PIN22:  PWM A8                              CTX1  | PIN22:
  *    PIN23:  PWM A9  MCLK1                       CRX1  | PIN23:
- *    PIN24:  PWM A10                       SCL2        | PIN24:
- *    PIN25:  PWM A11                       SDA2        | PIN25:
- *    PIN26:      A12             MOSI1                 | PIN26:
- *    PIN27:      A13             SCK1                  | PIN27:
- *    PIN28:  PWM                       RX7             | PIN28:
- *    PIN29:  PWM                       TX7             | PIN29:
- *    PIN30:                                      CRX3  | PIN30:
- *    PIN31:                                      CTX3  | PIN31:
- *    PIN32:          OUT1B                             | PIN32:
+ *    PIN24:  PWM A10                       SCL2        | PIN24: DEC1
+ *    PIN25:  PWM A11                       SDA2        | PIN25: DEC1
+ *    PIN26:      A12             MOSI1                 | PIN26: DEC1
+ *    PIN27:      A13             SCK1                  | PIN27: DEC2
+ *    PIN28:  PWM                       RX7             | PIN28: DEC2
+ *    PIN29:  PWM                       TX7             | PIN29: DEC2
+ *    PIN30:                                      CRX3  | PIN30: DEC1/2
+ *    PIN31:                                      CTX3  | PIN31: Button9
+ *    PIN32:          OUT1B                             | PIN32: LED9
  *    PIN33:  PWM     MCLK2                             | PIN33:
  *    PIN34:                            RX8             | PIN34:
  *    PIN35:                            TX8             | PIN35:
@@ -83,43 +83,43 @@ MIDI board for preset sounds:
 NAME:   TYPE:   ASSIGNMENT:
 VDD:    ?V
 GND:    GND
-RX:     Serial
-TX:     Serial
-GPIO1:   GPIO
+RX:     Serial     ?
+TX:     Serial     Pin1
+GPIO1:   GPIO      Pin9
 
 DPAD buttoms for GUI:
 NAME:   TYPE:   ASSIGNMENT:
 VDD:    5V
 GND:    GND
-GPIO1:   GPIO
-GPIO2:   GPIO
-GPIO3:   GPIO
-GPIO4:   GPIO
+GPIO1:   GPIO     Pin2
+GPIO2:   GPIO     Pin3
+GPIO3:   GPIO     Pin4
+GPIO4:   GPIO     Pin5
 
 LCD for GUI:
 NAME:   TYPE:   ASSIGNMENT:
 VDD:    5V
 GND:    GND
-SDA:    I2C
-SCL:    I2C
+SDA:    I2C       Pin18
+SCL:    I2C       Pin19
 
-Measure matrix decoders for buttons and leds
+Measure matrix decoders for buttons and LEDs
 NAME:    TYPE:   ASSIGNMENT:
 VDD:     5V
 GND:     GND
-GPIO1:   GPIO
-GPIO2:   GPIO
-GPIO3:   GPIO
-GPIO4:   GPIO
-GPIO5:   GPIO
-GPIO6:   GPIO
-GPIO7:   GPIO
-GPIO8:   GPIO
-GPIO9:   GPIO
-GPIO10:  GPIO
-GPIO11:  GPIO
-GPIO12:  GPIO
-GPIO13:  GPIO
+GPIO1:   GPIO       Pin24
+GPIO2:   GPIO       Pin25
+GPIO3:   GPIO       Pin26
+GPIO4:   GPIO       Pin27
+GPIO5:   GPIO       Pin28
+GPIO6:   GPIO       Pin29
+GPIO7:   GPIO       Pin30
+GPIO8:   GPIO       Pin31
+GPIO9:   GPIO       Pin32
+GPIO10:  GPIO       Pin8
+GPIO11:  GPIO       Pin10
+GPIO12:  GPIO       Pin11
+GPIO13:  GPIO       Pin12
 
 Serial Flash Memory for custom sounds:
 ** CANNOT BE CHANGED **
@@ -140,9 +140,9 @@ PT8211 DAC for custom sounds:
 NAME:   TYPE:   ASSIGNMENT:
 VDD:    3.3V
 GND:    GND
-BCK:    I2s
-WS:     I2s
-DIN:    I2s
+BCK:    I2s       Pin21
+WS:     I2s       Pin20
+DIN:    I2s       Pin7
 
 Onboard SD card reader for custom sounds and tracks:
 ** CANNOT BE CHANGED **
