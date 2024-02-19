29dedbb3ba38baa2325894dd5032b7bc2617a49c
Evan Rovelli <erovelli@umass.edu>
2024-02-19 02:23:57 -0500

    added more info to pin assignments + approve current pins

--- main/main.h
+++ main/main.h
@@ -15,14 +15,14 @@
  *    P:  CAN
  **************************  HEADERS  **************************
  *    PINOUT  R   O   Y           G     B   I     P     | PINOUT
- *    PIN0:   PWM                 CS1   RX1       CRX2  | PIN0: empty(For MIDI)
- *    PIN1:   PWM                 MISO1 TX1       CTX2  | PIN1: MIDI
+ *    PIN0:   PWM                 CS1   RX1       CRX2  | PIN0: empty(For MIDI) - RX
+ *    PIN1:   PWM                 MISO1 TX1       CTX2  | PIN1: MIDI - TX
  *    PIN2:   PWM     OUT2                              | PIN2: DPAD
  *    PIN3:   PWM     LRCLK2                            | PIN3: DPAD
  *    PIN4:   PWM     BCLK2                             | PIN4: DPAD
  *    PIN5:   PWM     IN2                               | PIN5: DPAD
  *    PIN6:   PWM     OUT1D                             | PIN6:
- *    PIN7:   PWM     OUT1A             RX2             | PIN7: DAC
+ *    PIN7:   PWM     OUT1A             RX2             | PIN7: DAC - DIN
  *    PIN8:   PWM     IN1               TX2             | PIN8: DEC3
  *    PIN9:   PWM     OUT1C                             | PIN9: MIDI
  *    PIN10:  PWM     MQSR        CS                    | PIN10: DEC3
@@ -33,10 +33,10 @@
  *    PIN15:  PWM A1  S/PDIF IN         RX3             | PIN15:
  *    PIN16:      A2                    RX4 SCL1        | PIN16:
  *    PIN17:      A3                    TX4 SDA1        | PIN17: 
- *    PIN18:  PWM A4                        SDA         | PIN18: LCD
- *    PIN19:  PWM A5                        SCL         | PIN19: LCD
- *    PIN20:      A6  LRCLK1            TX5             | PIN20: DAC
- *    PIN21:      A7  BCLK1             RX5             | PIN21: DAC
+ *    PIN18:  PWM A4                        SDA         | PIN18: LCD - SDA
+ *    PIN19:  PWM A5                        SCL         | PIN19: LCD - SCL
+ *    PIN20:      A6  LRCLK1            TX5             | PIN20: DAC - WS
+ *    PIN21:      A7  BCLK1             RX5             | PIN21: DAC - BCK
  *    PIN22:  PWM A8                              CTX1  | PIN22:
  *    PIN23:  PWM A9  MCLK1                       CRX1  | PIN23:
  *    PIN24:  PWM A10                       SCL2        | PIN24: DEC1
@@ -59,20 +59,20 @@
  *    PIN41:      A17                                   | PIN41:
  *    PINOUT  R   O   Y           G     B   I     P     | PINOUT
  *    *********************  BOTTOM PADS  **********************
- *    PINOUT  R   LY  G     LB    B   I                 | PINOUT
- *    PIN42:  PWM     MISO2 DAT1                        | PIN42:
- *    PIN43:  PWM     MOSI2 DAT0                        | PIN43:
- *    PIN44:  PWM     CS2   CLK       SDA1              | PIN44:
- *    PIN45:  PWM     SCK2  CMD       SCL1              | PIN45:
- *    PIN46:  PWM           DAT3  RX5                   | PIN46:
- *    PIN47:  PWM           DAT2  TX5                   | PIN47:
- *    PIN48:      CS0             RX8                   | PIN48:
- *    PIN49:      D1  SCK2                              | PIN49:
- *    PIN50:      D2  MOSI2                             | PIN50:
+ *    PINOUT  R   LY  G     LB    B   I                 | PINOUT                                
+ *    PIN42:  PWM     MISO2 DAT1                        | PIN42:    SD READER - DAT1
+ *    PIN43:  PWM     MOSI2 DAT0                        | PIN43:    SD READER - DAT0
+ *    PIN44:  PWM     CS2   CLK       SDA1              | PIN44:    SD READER - CLK
+ *    PIN45:  PWM     SCK2  CMD       SCL1              | PIN45:    SD READER - CMD
+ *    PIN46:  PWM           DAT3  RX5                   | PIN46:    SD READER - DAT3
+ *    PIN47:  PWM           DAT2  TX5                   | PIN47:    SD READER - DAT2
+ *    PIN48:      CS0             RX8                   | PIN48:    FLASH MEM - CS0
+ *    PIN49:      D1  SCK2                              | PIN49:    FLASH MEM - D1
+ *    PIN50:      D2  MOSI2                             | PIN50:    FLASH MEM - D2
  *    PIN51:  PWM CS1                 SCL1              | PIN51:
- *    PIN52:      D0              RX1                   | PIN52:
- *    PIN53:      CLK             TX1                   | PIN53:
- *    PIN54:  PWM D3  MISO2                             | PIN54:
+ *    PIN52:      D0              RX1                   | PIN52:    FLASH MEM - D0
+ *    PIN53:      CLK             TX1                   | PIN53:    FLASH MEM - CLK
+ *    PIN54:  PWM D3  MISO2                             | PIN54:    FLASH MEM - D3
  *    PINOUT  R   LY  G     LB    B   I                 | PINOUT
  */
 
@@ -81,11 +81,11 @@ HARDWARE PIN ALLOCATION REQUIREMENTS
 
 MIDI board for preset sounds:
 NAME:   TYPE:   ASSIGNMENT:
-VDD:    ?V
-GND:    GND
-RX:     Serial     ?
-TX:     Serial     Pin1
-GPIO1:   GPIO      Pin9
+VDD:    ?V      
+GND:    GND     GND
+RX:     Serial  PIN0
+TX:     Serial  PIN1
+GPIO1:   GPIO   PIN9
 
 DPAD buttoms for GUI:
 NAME:   TYPE:   ASSIGNMENT:
@@ -96,12 +96,6 @@ GPIO2:   GPIO     Pin3
 GPIO3:   GPIO     Pin4
 GPIO4:   GPIO     Pin5
 
-LCD for GUI:
-NAME:   TYPE:   ASSIGNMENT:
-VDD:    5V
-GND:    GND
-SDA:    I2C       Pin18
-SCL:    I2C       Pin19
 
 Measure matrix decoders for buttons and LEDs
 NAME:    TYPE:   ASSIGNMENT:
@@ -121,40 +115,45 @@ GPIO11:  GPIO       Pin10
 GPIO12:  GPIO       Pin11
 GPIO13:  GPIO       Pin12
 
+LCD for GUI:
+NAME:   TYPE:   ASSIGNMENT:
+VDD:    5V      5V
+GND:    GND     GND
+SDA:    I2C     PIN18
+SCL:    I2C     PIN19
+
 Serial Flash Memory for custom sounds:
 ** CANNOT BE CHANGED **
 NAME:       TYPE:   ASSIGNMENT:
-VDD:        3.3
-VSS:        GND
-CE#:        CS0
-SCLK:       CLK
-SI/SIO[0]:  D0
-SO/SIO[1]:  D1
-SIO[2]:     D2
-SIO[3]:     D3
-
-
+VDD:        3.3     3.3V
+VSS:        GND     GND
+CE#:        CS0     PIN48
+SCLK:       CLK     PIN53
+SI/SIO[0]:  D0      PIN52
+SO/SIO[1]:          PIN49
+SIO[2]:     D2      PIN50
+SIO[3]:     D3      PIN54
 
 PT8211 DAC for custom sounds:
 ** UNCERTAIN CAN BE CHANGED **
 NAME:   TYPE:   ASSIGNMENT:
-VDD:    3.3V
-GND:    GND
-BCK:    I2s       Pin21
-WS:     I2s       Pin20
-DIN:    I2s       Pin7
+VDD:    3.3V      3.3V
+GND:    GND       GND
+BCK:    I2s       PIN21
+WS:     I2s       PIN20
+DIN:    I2s       PIN7
 
 Onboard SD card reader for custom sounds and tracks:
 ** CANNOT BE CHANGED **
 NAME:   TYPE:   ASSIGNMENT:
-VDD:    3.3
-GND:    GND
-CMD:    FLEXIO
-CLK:    FLEXIO
-DAT0:   FLEXIO
-DAT1:   FLEXIO
-DAT2:   FLEXIO
-DAT3:   FLEXIO
+VDD:    3.3V    3.3V
+GND:    GND     GND
+CMD:    FLEXIO  PIN45
+CLK:    FLEXIO  PIN44
+DAT0:   FLEXIO  PIN43
+DAT1:   FLEXIO  PIN42
+DAT2:   FLEXIO  PIN47
+DAT3:   FLEXIO  PIN46
 
 
 */
